90ef386e672249e8d83f523c3a25a26095242b75
Andrey Yamangulov <yamangulov@gmail.com>
2022-07-02 12:22:07 +0300

    ДЗ 5.5 выполнено, на проверку

---
+++ virtualization/5.5/src/terraform/provider.tf
@@ -0,0 +1,16 @@
+# Provider
+terraform {
+  required_providers {
+    yandex = {
+      source = "yandex-cloud/yandex"
+    }
+  }
+}
+
+provider "yandex" {
+#  service_account_key_file = "key.json"
+  token = ""
+  cloud_id  = "${var.yandex_cloud_id}"
+  folder_id = "${var.yandex_folder_id}"
+  zone = "ru-central1-a"
+}
---
+++ virtualization/5.5/src/terraform/variables.tf
@@ -0,0 +1,17 @@
+# Заменить на ID своего облака
+# https://console.cloud.yandex.ru/cloud?section=overview
+variable "yandex_cloud_id" {
+  default = "b1gioclo0tmo2u4giv3r"
+}
+
+# Заменить на Folder своего облака
+# https://console.cloud.yandex.ru/cloud?section=overview
+variable "yandex_folder_id" {
+  default = "b1g9qk8kfl0vmmdrjtrt"
+}
+
+# Заменить на ID своего образа
+# ID можно узнать с помощью команды yc compute image list
+variable "centos-7-base" {
+  default = "fd8h4lc7vre44d6fb1rc"
+}
