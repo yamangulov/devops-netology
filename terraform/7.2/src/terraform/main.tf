97d8433591b2012ac9cdee8525c3f80ed5ca273e
Andrey Yamangulov <yamangulov@gmail.com>
2022-08-16 00:11:50 +0300

    ДЗ 7.2 выполнено, на проверку

---
+++ terraform/7.2/src/terraform/main.tf
@@ -0,0 +1,51 @@
+provider "yandex" {
+  token     = "${var.YC_TOKEN}"
+  cloud_id  = "${var.YC_CLOUD_ID}"
+  folder_id = "${var.YC_FOLDER_ID}"
+  zone      = "ru-central1-a"
+}
+
+resource "yandex_compute_instance" "vm-1" {
+  name = "terraform1"
+
+  resources {
+    cores  = 2
+    memory = 2
+  }
+
+  boot_disk {
+    initialize_params {
+      image_id = "fd89jk9j9vifp28uprop"
+    }
+  }
+
+  network_interface {
+    subnet_id = yandex_vpc_subnet.subnet-1.id
+    nat       = true
+  }
+
+  metadata = {
+    ssh-keys = "ubuntu:${file("~/.ssh/id_rsa.pub")}"
+  }
+}
+
+
+resource "yandex_vpc_network" "network-1" {
+  name = "network1"
+}
+
+resource "yandex_vpc_subnet" "subnet-1" {
+  name           = "subnet1"
+  zone           = "ru-central1-a"
+  network_id     = yandex_vpc_network.network-1.id
+  v4_cidr_blocks = ["192.168.10.0/24"]
+}
+
+output "internal_ip_address_vm_1" {
+  value = yandex_compute_instance.vm-1.network_interface.0.ip_address
+}
+
+
+output "external_ip_address_vm_1" {
+  value = yandex_compute_instance.vm-1.network_interface.0.nat_ip_address
+}
---
+++ terraform/7.2/src/terraform/vars.tf
@@ -0,0 +1,11 @@
+variable "YC_TOKEN" {
+  default = ""
+}
+
+variable "YC_CLOUD_ID" {
+  default = ""
+}
+
+variable "YC_FOLDER_ID" {
+  default = ""
+}
